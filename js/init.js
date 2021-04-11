64eb32896059bed685f9101d9dc02ed4501a82ad
proevedorPrac <74282825+proevedorPrac@users.noreply.github.com>
2021-04-10 20:13:26 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,9 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyCCe9VhUah-nsqU2-czRTpNDl8dQOtYeY0",
+    authDomain: "adaniot-4ed68.firebaseapp.com",
+    projectId: "adaniot-4ed68",
+    storageBucket: "adaniot-4ed68.appspot.com",
+    messagingSenderId: "674321946791",
+    appId: "1:674321946791:web:ebd7995150abb1646da382"
 });
